d498ce734072c99b4dc10fe967d6ba79f3ffe96c
Sam Legrand <sam.legrand@student.uantwerpen.be>
2020-11-01 19:55:31 +0100

    Update for writing bytearray

--- dataset/preprocess.py
+++ dataset/preprocess.py
@@ -2,13 +2,20 @@ from lxml import etree
 
 context = etree.iterparse('./dataset/Posts.xml', events=('end',), tag='row')
 
-f = open('./dataset/subset.xml', 'w')
+f = open('./dataset/subset.xml', 'wb')
 
 i = 0
+# lines = []
+my_bytes = bytearray()
 for action, elem in context:
     i += 1
     new_row = etree.Element('post', attrib={attribute: elem.attrib[attribute] for attribute in ['Id', 'ParentId', 'AcceptedAnswerId', 'CreationDate', 'LastEditDate', 'Body', 'LastEditorDisplayName', 'Title', 'Tags'] if attribute in elem.attrib})
-    f.write(etree.tostring(new_row).decode('utf-8'))
-    f.write('\n')
-    if i == 10000:
+    # lines.append(etree.tostring(new_row) + b'\x0a')
+    my_bytes.extend(etree.tostring(new_row) + b'\x0a')
+    # bytestring += etree.tostring(new_row) + b'\x0a'
+    # f.write(etree.tostring(new_row) + b'\x0a')
+    if i % 100000 == 0:
+        f.write(my_bytes)
+        # break
+    if i == 1000000:
         break
