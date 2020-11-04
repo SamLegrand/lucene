54d02d3cb243067f3eef7c96dd7f03de0de38f77
SamLegrand <sam.legrand@student.uantwerpen.be>
2020-11-04 14:25:25 +0100

    New subset generator

--- dataset/preprocess.py
+++ dataset/preprocess.py
@@ -3,7 +3,6 @@ from lxml import etree
 context = etree.iterparse('./dataset/Posts.xml', events=('end',), tag='row')
 
 i = 0
-# lines = []
 for action, elem in context:
     i += 1
     new_row = etree.Element('post', attrib={attribute: elem.attrib[attribute] for attribute in ['Id', 'ParentId', 'AcceptedAnswerId', 'CreationDate', 'LastEditDate', 'Body', 'LastEditorDisplayName', 'Title', 'Tags'] if attribute in elem.attrib})
@@ -14,8 +13,5 @@ for action, elem in context:
     doc = open('./dataset/docs/%s.xml' % filenum, 'ab')
     doc.write(etree.tostring(new_row) + b'\x0a')
     doc.close()
-    # lines.append(etree.tostring(new_row) + b'\x0a')
-    # bytestring += etree.tostring(new_row) + b'\x0a'
-    # f.write(etree.tostring(new_row) + b'\x0a')
     if i == 10000:
         break
