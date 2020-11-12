d6f7bdc4ee31f47e0122fd743603f9feb4b1f106
Sam Legrand <sam.legrand@student.uantwerpen.be>
2020-11-12 10:37:51 +0100

    Remove helper scripts

--- dataset/combine.py
+++
@@ -1,9 +0,0 @@
-import glob
-
-fout = open('./dataset/processed.xml', 'wb')
-fout.write(b'<posts>')
-for filename in glob.glob("./dataset/docs/*.xml"):
-    with open(filename, 'rb') as fin:
-        fout.write(fin.read())
-fout.write(b'</posts>')
-fout.close()
